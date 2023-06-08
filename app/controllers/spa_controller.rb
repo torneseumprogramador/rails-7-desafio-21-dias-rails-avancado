1e8a94cb29900161750cbbfcc599a9e04a7a07f7
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-06-08 06:39:54 -0300

    iniciando com stimulus

---
+++ app/controllers/spa_controller.rb
@@ -0,0 +1,3 @@
+class SpaController < ApplicationController
+  def index;end
+end
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "<h1>O texto que vem do controlador do stimulus</h1>"
+    this.element.innerHTML = "<h1>O texto que vem do controlador do stimulus</h1>"
   }
 }
