862f30e825b7d7cff65f29c61a175eb76dff98bb
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-06-09 06:32:38 -0300

    estratégia 3

--- app/javascript/controllers/clientes_controller.js
+++ app/javascript/controllers/clientes_controller.js
@@ -7,21 +7,36 @@ export default class extends Controller {
 
   async loadClientes() {
     const clientes = await (await fetch('http://localhost:3000/clientes.json')).json();
-    this.displayClientes(clientes);
+    const html = this.renderClientesTable(clientes);
+    this.tableTarget.innerHTML = html;
   }
 
-  displayClientes(clientes) {
-    const tbody = document.getElementById('clientes-table');
-    clientes.forEach(cliente => {
-      const html = `
-        <tr>
-          <td>${cliente.id}</td>
-          <td>${cliente.nome}</td>
-          <td>${cliente.telefone}</td>
-          <td>${cliente.endereco}</td>
-        </tr>
-      `;
-      tbody.innerHTML += html;
-    });
-  }  
+  renderClientesTable(clientes) {
+    return `
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Telefone</th>
+            <th>Endereço</th>
+            <th>Data de Criação</th>
+            <th>Data de Atualização</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${clientes.map(cliente => `
+            <tr>
+              <td>${cliente.id}</td>
+              <td>${cliente.nome}</td>
+              <td>${cliente.telefone}</td>
+              <td>${cliente.endereco}</td>
+              <td>${cliente.created_at}</td>
+              <td>${cliente.updated_at}</td>
+            </tr>
+          `).join("")}
+        </tbody>
+      </table>
+    `;
+  }
 }
