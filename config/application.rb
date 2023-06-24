555a812a06e2068295e4a6144f9bba80aae68fe8
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-06-24 05:58:43 -0300

    alterando via metaprogramação

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,18 @@ module LayoutRails7
     # config.autoload_paths += %W(#{config.root}/app/entidades)
     # config.autoload_paths += %W(#{config.root}/app/repositorios)
 
+    config.after_initialize do
+      Devise::SessionsController.class_eval do
+          def create
+              debugger
+              self.resource = warden.authenticate!(auth_options)
+              set_flash_message!(:notice, :signed_in)
+              sign_in(resource_name, resource)
+              yield resource if block_given?
+              respond_with resource, location: after_sign_in_path_for(resource)
+          end
+      end
+    end
+
   end
 end
